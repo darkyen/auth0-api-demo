bcf6d624b3738c866f2f68df229318d24afa68dc
Just a random person <abhishek@hingnikar.com>
2017-03-14 11:25:03 -0700

    Update rule.js

--- rule.js
+++ rule.js
@@ -4,9 +4,10 @@ function (user, context, callback) {
       console.log(context, user);
       const apiIdentifier = context.request.query.audience;
       if (user.email === configuration.ADMIN_EMAIL) {
-         console.log("it should add email");
-         context.accessToken[ + '/role'] = 'admin';
-         context.idToken[+ '/role'] = 'admin';
+         console.log("it should add role");
+         // Just make sure its santized
+         context.accessToken[ apiIdentifier + '/role'] = 'admin';
+         context.idToken[apiIdentifier + '/role'] = 'admin';
       }
     }    
     return callback(null, user, context);
